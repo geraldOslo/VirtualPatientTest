80d895872fba6b80044babc65b8779f5be8d8325
Gerald Torgersen <57004280+geraldOslo@users.noreply.github.com>
2024-09-21 08:09:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,17 @@
 import streamlit as st
-import openai
+from openai import AzureOpenAI
 
-# Set up the OpenAI client to use Azure OpenAI services
-openai.api_type = "azure"
-openai.api_base = st.secrets["OPENAI_API_BASE"]         # Your Azure OpenAI Endpoint
-openai.api_version = st.secrets["OPENAI_API_VERSION"]   # API version
-openai.api_key = st.secrets["OPENAI_API_KEY"]           # API key
+# Create an AzureOpenAI client
+client = AzureOpenAI(
+    azure_endpoint=st.secrets["OPENAI_API_BASE"],         # Your Azure OpenAI Endpoint
+    api_version=st.secrets["OPENAI_API_VERSION"],         # API version
+    api_key=st.secrets["OPENAI_API_KEY"],                 # API key
+)
 
 # Show title and description.
 st.title("💬 SimSamBot")
 st.write(
-    "Prototype for intern testing"
+    "Prototype til intern testing"
 )
 
 # Create a session state variable to store the chat messages.
@@ -23,7 +24,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Create a chat input field to allow the user to enter a message.
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Hei tannlege?"):
 
     # Store and display the user's message.
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -31,8 +32,8 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
 
     # Generate a response using the Azure OpenAI API.
-    response = openai.ChatCompletion.create(
-        engine=st.secrets["OPENAI_DEPLOYMENT_NAME"],  # Uses your assistant ID
+    response = client.chat.completions.create(
+        model=st.secrets["OPENAI_DEPLOYMENT_NAME"],  # Use your deployment name as the model
         messages=st.session_state.messages,
         temperature=0.7,
         max_tokens=500,
@@ -44,7 +45,7 @@ if prompt := st.chat_input("What is up?"):
         response_text = st.empty()
         full_response = ""
         for chunk in response:
-            chunk_message = chunk['choices'][0]['delta'].get('content', '')
+            chunk_message = chunk.choices[0].delta.get('content', '')
             full_response += chunk_message
             response_text.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
